f71640a9a09ce652844b37e098b1d41ddccb8dd3
jlperla <jesseperla@gmail.com>
2017-09-07 10:00:19 -0700

    Tomlab tutorials moved to separate repository.

--- tests/tomlab/tomlab_tutorial/portfolio_constraint.m
+++
@@ -1,3 +0,0 @@
-function c = portfolio_constraint(x, Prob)
-    c = (1/2)*sum( log(Prob.sigma.^2) - log(x.^2) ) - Prob.kappa;
-end
--- tests/tomlab/tomlab_tutorial/portfolio_objective.m
+++
@@ -1,3 +0,0 @@
-function f = portfolio_objective(x, Prob)
-    f = sum((x.^2).*(Prob.alpha .^2));
-end
